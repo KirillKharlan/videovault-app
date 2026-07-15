09de2225b0dd57f1c5a049dd5ac563d82f460f6e
KirillKharlan <151565579+KirillKharlan@users.noreply.github.com>
2026-07-15 10:20:17 +0300

    updt

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,12 +6,13 @@ plugins {
 
 android {
     namespace = "com.example.videovault"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -23,6 +24,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -42,4 +44,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
 }
